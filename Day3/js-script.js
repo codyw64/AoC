925edf929c2b40582d422d748188247dc0267f5c
Cody <whearcody@gmail.com>
2022-01-06 13:25:38 +0000

    DId the first part of the day 3 part 2 challenge! Involved lots of loops, recursive functions, arrays, etc. LOoking forward to the second part and finishing the day off!

--- Day3/js-script.js
+++ Day3/js-script.js
@@ -12,52 +12,50 @@ let arr = `00100
 01010`;
 
 function banana(arr) {
+    //oxygen rating
     let newArr = arr.split("\n");
-    //oxygen
-    oxygen();
-    function oxygen() {
-        let count0 = 0, count1 = 0;        
+    let zeroCount = 0, oneCount = 0;
+    for (let x = 0; x < newArr[0].length; x++) {
+        oxygen(x);
+    }
+
+    function oxygen(number) {
         for (let i = 0; i < newArr.length; i++) {
-            for (let j = 0; j < 2; j++) {
-                if (newArr[i].charAt(j) == 0) {
-                    count0++;
-                } else if (newArr[i].charAt(j) == 1) {
-                    count1++;
-                }
+            if (newArr[i].charAt(number) == 0) {
+                zeroCount += 1;
+            } else {
+                oneCount += 1;
             }
-    }
-    oxygen1();
-    function oxygen1() {
-        if (count0 > count1 || count0 == count1) {
-        for (let i = 0; i < newArr.length; i++) {
-            for (let j = 0; j < 1; j++) {
-                if (newArr[i].charAt(j) == 1) {
-                    newArr.splice(i, 1);
-                    oxygen1();
+        }
+        
+        oxygen2();
+        function oxygen2() {
+           for (let i = 0; i < newArr.length; i++) {
+                if (zeroCount < oneCount) {
+                    if (newArr[i].charAt(number) == 0) {
+                        newArr.splice(i, 1);
+                        oxygen2();
+                    }
+                } else if (oneCount < zeroCount) {
+                    if (newArr[i].charAt(number) == 1) {
+                        newArr.splice(i, 1);
+                        oxygen2();
+                    }
+                } else if (oneCount == zeroCount) {
+                    if (newArr[i].charAt(number) == 0) {
+                        newArr.splice(i, 1);
+                        oxygen2();
+                    }
                 }
-            }
+            } 
+        }
+        zeroCount = 0, oneCount = 0;
     }
-    } else if (count0 < count1) {
-        for (let i = 0; i < newArr.length; i++) {
-            for (let j = 0; j < 1; j++) {
-                if (newArr[i].charAt(j) == 0) {
-                    newArr.splice(i, 1);
-                    oxygen1();
-                }
-            }
-    }
-    }
-    
-    }
-    console.log(newArr);
-    }
-
-
-
-    //co2
-
-
+    const ORB = newArr[0];
+    const oxygenRating = ORB.charAt(ORB.length - 1) * 1**1 + ORB.charAt(ORB.length - 2) * 2**1 + ORB.charAt(ORB.length - 3) * 2**2 + ORB.charAt(ORB.length - 4) * 2**3 + ORB.charAt(ORB.length - 5) * 2**4 + ORB.charAt(ORB.length - 6) * 2**5;  
+    console.log(oxygenRating);
 }
 
-
 banana(arr);
+
+
